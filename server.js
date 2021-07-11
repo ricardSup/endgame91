5ff57eadb9b015974fbe7fdd086d9a7523feb081
richard <richardlim97@rocketmail.com>
2021-07-12 01:59:37 +0700

    update production mode

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const hostname = 'localhost';
 const PORT = process.env.PORT || 3000;
 
 // mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://endgame9:endgame9@cluster0.ztxf1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 
